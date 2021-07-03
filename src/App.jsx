bf6429c5bcf6369a45618fb57ee230ad6687446f
Guilherme Senna Cruz <53920452+GuilhermeSenna@users.noreply.github.com>
2021-07-03 01:39:13 -0300

    Tela inicial iniciada - Uso da lib material-ui - Adição do navbar, textos iniciais e botão flutuante - Modularização em Header, App e Footer - Background adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+import "./style.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+      <h1>Lista de candidatos</h1>
+      <h2> Nenhum candidato cadastrado ainda...</h2>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Box from "@material-ui/core/Box";
+import "./style.css";
 
 import App from "./App";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <div>
-    <h1>teste</h1>
+    <Header />
     <App />
+    <Footer />
   </div>,
   rootElement
 );
