dbc7cfaf8497f368acb809ced974bda4977141c9
Guilherme Senna Cruz <53920452+GuilhermeSenna@users.noreply.github.com>
2021-07-03 00:36:34 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>teste</h1>
+    <App />
+  </div>,
+  rootElement
+);
