09e40023053a0f07bd72a37ea1dbbe85312d8941
Matheus Cavalcante <76076019+cavalcant3@users.noreply.github.com>
2021-07-05 03:44:53 -0300

    estilização do form e button Utilizado material ui

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function Add() {
   return (
@@ -6,14 +8,24 @@ export default function Add() {
       <form action="/action_page.php">
         <label for="fname">First name:</label>
         <br />
-        <input type="text" id="fname" name="fname" value="John" />
+        <div className="TextField">
+          <TextField type="text" id="fname" name="fname" value="John" />
+        </div>
         <br />
         <label for="lname">Last name:</label>
         <br />
-        <input type="text" id="lname" name="lname" value="Doe" />
+        <div className="TextField">
+          <TextField type="text" id="lname" name="lname" value="Doe" />
+        </div>
         <br />
         <br />
-        <input type="submit" value="Submit" />
+        {/* <input type="submit" value="Submit" /> */}
+        <Button variant="outlined" color="secondary">
+          Submit
+        </Button>
+        {/* você pode alternar a cor entre algumas opções,
+         essa foi a que mais se destaca.
+         Podemos reaproveitar */}
       </form>
     </div>
   );
